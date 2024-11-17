aa6aa793846f55fb62fb93e2fbac4e0ac11230a7
liweier123 <3603674798@qq.com>
2024-11-17 12:54:48 +0800

    Site updated: 2024-11-17 12:54:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/15/hello-world/"];function toRandomPost(){
+var posts=["2024/11/15/hello-world/","2024/11/17/企业网络搭建笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
